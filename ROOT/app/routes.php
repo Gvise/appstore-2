dd9323185971d5ca71cb9971158a608aec3fa992
Chan Nyein <reven.rn@gmail.com>
2016-07-06 10:57:44 +0630

    ready to use!

--- ROOT/app/routes.php
+++ ROOT/app/routes.php
@@ -8,18 +8,18 @@
 //	'**' => '.++',
 //	''   => '[^/\.]++'
 
-App::map('GET', 'index', function() {
-	$rows = DB::raw('select * from table1');
-	// DB::raw('insert into table1(name) values(?)', ['ch'], function($count, $stmt) {
-	// 	if($count) {
-	// 		echo 'Success';
-	// 	} else {
-	// 		echo 'Failure';
-	// 	}
-	// });
-	View::render('index', [
-        'rows' => $rows
-    ]);
-}, 'indexPage');
+App::map('GET', 'select', function() {
+	$rows = DB::raw('select * from '. Config::get('db.table'));
+	print_r($rows);
+}, 'get');
 
-App::map('GET','greet/[i:name]', 'Ctrl1@greet');
+App::map('GET', 'insert/[:name]', function($name) {
+	DB::raw('insert into ' . Config::get('db.table') . '(name) values(?)', [$name], function($count, $stmt) {
+		if($count) {
+			echo 'Successful!';
+		}
+		else {
+			echo 'Failure';
+		}
+	});
+});
--- ROOT/dependencies/config.php
+++ ROOT/dependencies/config.php
@@ -1,9 +1,10 @@
 <?php
 return [
-    'basepath' => '',
+    'basepath' => 'appstore/',
     'views' => __ROOT__.'templates/',
     'db' => [
         'host' => 'localhost',
+        'table' => 'table1',
         'db' => 'test',
         'user' => 'root',
         'password' => ''
